1ba3e5dc393eab71d73a5e0c8fd759e4c5719e47
KaarleJ <117437182+KaarleJ@users.noreply.github.com>
2022-12-18 12:46:29 +0200

    2.15-20
    Vanha hakemisto muutettiin muotoon: 'osa2.15-2.20'. Tehty osien 2.15-2.20 tehtävät. Ohjelma vastaa osaa 2.20.

--- osa2.15-2.20/src/App.js
+++ osa2.15-2.20/src/App.js
@@ -1,10 +1,10 @@
 import { useState, useEffect } from 'react'
 import numberService from './services/numbers'
 
-const Filter = ({search, handleChange}) => <div>filter shown with<input value={search} onChange={handleChange}/></div>
-const Result = ({person, handleRemove}) => <p>{person.name} {person.number} <button onClick={() => handleRemove(person.id)}>delete</button></p>
-const Results = ({presentable, handleRemove}) => <div>{presentable.map(person => <Result key={person.number} person={person} handleRemove={handleRemove}/>)} </div>
-const NumberForm = ({submitAction, nameInput, numberInput, handleName, handleNumber}) => {
+const Filter = ({ search, handleChange }) => <div>filter shown with<input value={search} onChange={handleChange}/></div>
+const Result = ({ person, handleRemove }) => <p>{person.name} {person.number} <button onClick={() => handleRemove(person.id)}>delete</button></p>
+const Results = ({ presentable, handleRemove }) => <div>{presentable.map(person => <Result key={person.number} person={person} handleRemove={handleRemove}/>)} </div>
+const NumberForm = ({ submitAction, nameInput, numberInput, handleName, handleNumber }) => {
   return (
     <form onSubmit={submitAction}>
         <div>
@@ -20,32 +20,65 @@ const NumberForm = ({submitAction, nameInput, numberInput, handleName, handleNum
   )
 }
 
+const Notice = ({ message }) => {
+  const noticeStyle = {
+    color: message.color,
+    font: 20,
+    backgroundColor: '#E7E1E0',
+    borderStyle: 'solid',
+    borderRadius: 5,
+    padding: 10,
+    marginBottom: 10,
+  }
+  if (message.state === null) {
+    console.log('message is null', message.state)
+    return null
+  }
+  console.log('message is not null', message.state)
+  return (
+    <div style={noticeStyle}>
+      {message.state}
+    </div>
+
+  )
+}
+
 const App = () => {
 
   const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [search, setSearch ] = useState('')
+  const [notice, setNotice] = useState({state: null, color: 'green'})
+
+  useEffect(() => {
+    numberService.getAll().then(phoneBook => setPersons(phoneBook))
+  }, [])
 
   const handleNameChange = (event) => setNewName(event.target.value)
+
   const handleNumberChange = (event) => setNewNumber(event.target.value)
+
   const handleSearchChange = (event) => setSearch(event.target.value)
+
   const handleRemove = (id) => {
     const undesirable = persons.find(person => person.id===id)
     if (window.confirm(`Delete ${undesirable.name}`)){
       console.log('removing',id,undesirable)
       numberService.remove(id).then(() => {
       console.log('Setting new persons')
-      console.log(persons.filter(person => person.id !== id))
       setPersons(persons.filter(person => person.id !== id))
-      console.log(persons)
-    })
+      setNotice({state: `Removed ${undesirable.name} from the phonebook`, color: 'green'})
+      setTimeout(() => setNotice({state: null, color: 'green'}),5000)
+      
+    }).catch( () => {
+      setPersons(persons.filter(person => person.id !== id))
+      setNotice({state: `Information of ${undesirable.name} has already been removed from the server`,color: 'red'})
+      setTimeout(() => setNotice({state: null, color: 'green'}),5000)
+    }
+    )
     }
   }
-
-  useEffect(() => {
-    numberService.getAll().then(phoneBook => setPersons(phoneBook))
-  }, [])
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -63,6 +96,8 @@ const App = () => {
         setPersons(persons.map(person => person.name === newPerson.name ? newPerson : person))
         setNewName('')
         setNewNumber('')
+        setNotice({state: `${newPerson.name} number changed to ${newPerson.number}`, color: 'green'})
+        setTimeout(() => setNotice({state: null, color: 'green'}),5000)
         })
       }
     } else {
@@ -72,6 +107,8 @@ const App = () => {
         setPersons(persons.concat(returnedPerson))
         setNewNumber('')
         setNewName('')
+        setNotice({state: `Added ${newPerson.name} to the phonebook`, color: 'green'})
+        setTimeout(() => setNotice({state: null, color: 'green'}),5000)
       }
       )
     }
@@ -81,6 +118,7 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notice message={notice}/>
       <Filter search={search} handleChange={handleSearchChange}/>
       <h3>add a new</h3>
       <NumberForm submitAction={addPerson} nameInput={newName} numberInput={newNumber} handleName={handleNameChange} handleNumber={handleNumberChange}/>
